857286d20c65d83642d2f231308716e2a545fc12
Mikhail Shytsko <xfiive@mail.ru>
2024-07-23 21:56:49 +0300

    inital commit

---
+++ src/main/java/org/parser/wb_tshirt_parser/WbTshirtParserApplication.java
@@ -0,0 +1,13 @@
+package org.parser.wb_tshirt_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbTshirtParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WbTshirtParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/parser/wb_tshirt_parser/WbTshirtParserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.parser.wb_tshirt_parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbTshirtParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
