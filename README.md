e96c35b6dcb548ea9efe1331450e7153745e491b
Mikhail Shytsko <xfiive@mail.ru>
2024-07-31 00:02:46 +0300

    updated readme

--- README.md
+++ README.md
@@ -4,4 +4,13 @@ NO COMMERCIAL PROFIT, free for distribution and modification.
 
 Made only for convenience and learning certain technologies.
 
-Technology stack: Spring Boot, Spring Web, Spring Data, MongoDB, Jsoup (no longer relevant, as the site for parsing generated code through JS and had to abandon this technology), Selenium.
+Technology stack: Spring Boot, Spring Web, Spring Data, MongoDB, Jsoup (no longer relevant, as the site for parsing generated code through JS and had to abandon this technology), probably Selenium.
+
+Well, after several nights of searching, I still haven't solved my problems with Selenium. 
+I ran into the 
+```java.lang.NoSuchMethodError: ‘java.io.OutputStream org.openqa.selenium.chrome.ChromeDriverService$Builder.getLogOutput(java.lang.String)’ ```
+problem like a river into rocks( 
+
+I tried everything I could find on the Internet and GitHub threads with questions including, but I never came to an answer.
+
+I am freezing this project until better times.
--- src/main/java/org/parser/wb_goods_parser/parser/PageConsumer.java
+++ src/main/java/org/parser/wb_goods_parser/parser/PageConsumer.java
@@ -1,13 +1,13 @@
 package org.parser.wb_goods_parser.parser;
 
+import com.microsoft.playwright.*;
 import lombok.RequiredArgsConstructor;
+import okhttp3.OkHttpClient;
 import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,60 +17,62 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class PageConsumer {
 
     private static final Logger logger = LoggerFactory.getLogger(PageConsumer.class);
-
+    private final OkHttpClient httpClient = new OkHttpClient();
     @Value("${parser.search-postfix-page-num}")
     private String postfix;
-
     @Value("${parser.search-prefix-catalog}")
     private String prefix;
-
     @Value("${parser.base-url}")
     private String baseUrl;
-
     @Value("${parser.user-agent}")
     private String userAgent;
 
-    public Document consumePage(String request) {
+    public void consumePage(String request) {
         String startPageUrl = this.formRequestQuery(request);
-
         logger.info("Start page url created: {}", startPageUrl);
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+            Browser.NewContextOptions contextOptions = new Browser.NewContextOptions().setUserAgent(userAgent);
+            BrowserContext context = browser.newContext(contextOptions);
+            Page page = context.newPage();
+            page.navigate(startPageUrl);
 
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get(startPageUrl);
+            // Ждем, пока элементы загрузятся
+            page.waitForSelector(".catalog__content");
 
-        String pageSource = driver.getPageSource();
-        Document page = Jsoup.parse(pageSource);
+            // Получаем содержимое страницы
+            String pageContent = page.content();
+            Document document = Jsoup.parse(pageContent);
+            extractAndPrintMainContent(document);
 
-        extractAndPrintMainContent(page);
+            browser.close();
+            logger.info("Finished parsing.");
+        }
+    }
 
-        driver.quit();
+    private void extractAndPrintMainContent(@NotNull Document page) {
+        // Используем атрибутный селектор для поиска элементов с data-tag="card"
+        Elements productElements = page.select("div.product-card.catalog-card[data-tag=card]");
+        logger.info("Product elements size: {}", productElements.size());
+        for (Element productElement : productElements) {
+            String productLink = productElement.select("[data-tag=link]").attr("href");
+            String sellerName = productElement.select("[data-tag=brandName] .product-card__brand").text();
+            String productName = productElement.select("[data-tag=brandName] .product-card__name").text();
+            String productPrice = productElement.select("[data-tag=salePrice]").text() + " " + productElement.select("[data-tag=saleCurrency]").text();
 
-        return null;
+            System.out.printf("Product Link: %s%nSeller Name: %s%nProduct Name: %s%nProduct Price: %s%n", productLink, sellerName, productName, productPrice);
+        }
     }
 
     private @NotNull String formRequestQuery(String request) {
         String requestInUTF8 = URLEncoder.encode(request, StandardCharsets.UTF_8);
         return baseUrl + prefix + requestInUTF8 + postfix;
-    }
-
-    private void extractAndPrintMainContent(@NotNull Document page) {
-        Elements mainElements = page.select("main.main");
-        for (Element mainElement : mainElements) {
-            Elements allElements = mainElement.getAllElements();
-            for (Element element : allElements) {
-                String tagName = element.tagName();
-                String classes = element.className();
-                System.out.println("Tag: " + tagName + ", Classes: " + classes);
-            }
-        }
     }
 
     private boolean isPageEmpty(@NotNull Document page) {
